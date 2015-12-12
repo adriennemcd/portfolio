4bd3642cd707107a925245622a7322496789aa80
Adrienne McDonnell <adrienne.mcd.21@gmail.com>
2015-12-12 12:10:06 -0500

    fixed nav visibility issue

--- js/index.js
+++ js/index.js
@@ -1,5 +1,21 @@
 'use strict'
 
+$(document).ready(function() {
+    // run test on page load and window resize
+    checkSize();
+    $(window).resize(checkSize);
+});
+
+//Check whether the window is tablet or mobile based on nav icon visibility
+//Allows nav to be visible when window is resized large, even with toggling on smaller screens
+function checkSize(){
+    if ($(".menu-icon").css('display') == 'none'){
+    	$('.trigger').css('display', 'block');
+    } else {
+    	$('.trigger').css('display', 'none');
+    }
+}
+
 // Toggle menu in mobile site
 $('.menu-icon').click(function(){
 	$('.trigger').toggle();
