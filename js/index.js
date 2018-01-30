1a3655adad0d91d71dd9ce9a9b77dd16173b03af
adriennemcd <adrienne.mcd.21@gmail.com>
2018-01-30 17:11:03 -0500

    make html more semantic for better accessibility, add functionality for triggering hover state when tabbing through homepage

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
 'use strict'
 
-$(function() {
-    $('.posts__item').matchHeight();
-});
+// make sure the grid items have equal height
+$('.posts__item').matchHeight();
+
+// trigger grid item hover styles if tabbing through site
+$('.posts__link').on('focus focusout', function(e){
+  $('.posts__link').each(function(i, item) {
+      var parent = item.closest('.posts__item');
+      $(this).is(':focus') ? $(parent).addClass('hover') : $(parent).removeClass('hover');
+  });
+})
+
+
 
 
